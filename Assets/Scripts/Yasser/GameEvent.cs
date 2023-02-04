dd6713ea26f15a3e3e650ef8204c14312cb29ccf
Ahmed Yaser <ahmedyaserabbas@gmail.com>
2023-02-04 10:25:07 +0200

    Working on Events

---
+++ Assets/Scripts/Yasser/GameEvent.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "GameEvent")]
+public class GameEvent : ScriptableObject
+{
+    public List<GameEventListener> listeners = new List<GameEventListener>();
+
+    public void Raise()
+    {
+        foreach (GameEventListener listener in listeners)
+        {
+            listener.OnEventRaised();
+        }
+    }
+
+    public void RegisterListener(GameEventListener listener)
+    {
+        if (!listeners.Contains(listener))
+            listeners.Add(listener);
+    }
+
+    public void UnregisterListener(GameEventListener listener)
+    {
+        if (listeners.Contains(listener))
+            listeners.Remove(listener);
+    }
+}
---
+++ Assets/Scripts/Yasser/GameEventListener.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+    public GameEvent gameEvent;
+
+    public UnityEvent response;
+
+    private void OnEnable()
+    {
+        gameEvent.RegisterListener(this);
+    }
+
+    private void OnDisable()
+    {
+        gameEvent.UnregisterListener(this);
+    }
+
+    public void OnEventRaised()
+    {
+        response.Invoke();
+    }
+}
