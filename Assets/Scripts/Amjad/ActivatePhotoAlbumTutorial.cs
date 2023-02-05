b87eb9c1421c7f9da1cfb063ad57a4da428da226
Amjad Mohamed <amjadmohamed55@gmail.com>
2023-02-05 09:53:50 +0200

    Activate Tutorial On First Interaction

--- Assets/Scripts/Amjad/ActivatePhotoAlbumTutorial.cs
+++ Assets/Scripts/Amjad/ActivatePhotoAlbumTutorial.cs
@@ -6,14 +6,24 @@ using UnityEngine.UI;
 public class ActivatePhotoAlbumTutorial : MonoBehaviour
 {
     // can get the number of interactables with the event system if we gonna go with that solution 
-    public int NumberOfInteractsToActivateAlbum;
+    private static ActivatePhotoAlbumTutorial instance;
+
+    [HideInInspector] public int InteractsCnt;
+    [SerializeField] int NumberOfInteractsToActivateAlbum;
     [SerializeField] Button PhotoAlbum;
     [SerializeField] GameObject TutorialPanel;
     [SerializeField] GameObject Inventory;
 
 
-    private int InteractsCnt;
     private bool IsTutorialShown = false;
+
+    public static ActivatePhotoAlbumTutorial Instance { get => instance; set => instance = value; }
+
+    private void Awake()
+    {
+        if (!instance)
+            instance = this;
+    }
 
     private void Update()
     {
@@ -23,12 +33,6 @@ public class ActivatePhotoAlbumTutorial : MonoBehaviour
             PhotoAlbum.interactable = true;
             IsTutorialShown = true;
             Inventory.SetActive(true);
-        }
-
-        // this is a placeholder to test the tutorial functionality
-        if(Input.GetKeyDown(KeyCode.L))
-        {
-            InteractsCnt++;
         }
     }
 }
